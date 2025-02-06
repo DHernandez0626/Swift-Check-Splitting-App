cc7bc51fe2bd76f0c7969e306a2a212b51acfa9c
Danny Hernandez <192526703+DHernandez0626@users.noreply.github.com>
2025-02-05 21:15:19 -0500

    initial Files

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by danny hernandez on 2/5/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
